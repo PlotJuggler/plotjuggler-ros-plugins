b40db76f060adc91ad014a6f20fd032a127af9a7
Erwin Lejeune <erwin.lejeune@tii.ae>
2025-11-14 00:56:48 +0400

    Update dialog header for table selection

--- src/publisher_select_dialog.h
+++ src/publisher_select_dialog.h
@@ -1,11 +1,6 @@
 #pragma once
 #include <QDialog>
-#include <QLabel>
-#include <QListWidget>
-#include <QAbstractButton>
-#include <unordered_set>
-#include <map>
-#include <QCheckBox>
+#include <QStringList>
 
 #include "ui_publisher_select_dialog.h"
 
@@ -19,12 +14,15 @@ class PublisherSelectDialog : public QDialog
   Q_OBJECT
 private:
   Ui::PublisherSelect *_ui;
-  std::map<std::string, std::pair<QLabel *, QCheckBox *>> _topic_widgets;
 
 public:
   explicit PublisherSelectDialog(QWidget* parent = nullptr) : QDialog(parent), _ui(new Ui::PublisherSelect)
   {
     _ui->setupUi(this);
+    _ui->listTopics->verticalHeader()->setVisible(false);
+    QStringList labels;
+    labels.push_back("Topic name");
+    _ui->listTopics->setHorizontalHeaderLabels(labels);
   }
 
   Ui::PublisherSelect* ui()
