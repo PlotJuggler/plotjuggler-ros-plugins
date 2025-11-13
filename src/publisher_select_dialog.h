d521d3cd4f430a51e2b9bc3e465d032ec74b03c3
Erwin Lejeune <erwin.lejeune@tii.ae>
2025-11-14 00:52:56 +0400

    Add filter slot and widget storage to dialog

--- src/publisher_select_dialog.h
+++ src/publisher_select_dialog.h
@@ -4,19 +4,22 @@
 #include <QListWidget>
 #include <QAbstractButton>
 #include <unordered_set>
+#include <map>
+#include <QCheckBox>
 
 #include "ui_publisher_select_dialog.h"
 
 namespace Ui
 {
-class Ui_PublisherSelect;
+  class Ui_PublisherSelect;
 }
 
 class PublisherSelectDialog : public QDialog
 {
   Q_OBJECT
 private:
-  Ui::PublisherSelect* _ui;
+  Ui::PublisherSelect *_ui;
+  std::map<std::string, std::pair<QLabel *, QCheckBox *>> _topic_widgets;
 
 public:
   explicit PublisherSelectDialog(QWidget* parent = nullptr) : QDialog(parent), _ui(new Ui::PublisherSelect)
